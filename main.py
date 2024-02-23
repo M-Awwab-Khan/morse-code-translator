7e40e5220812c5de4f0affb9904c7cd8163495e3
Awwab <mawwabkhank2006@gmail.com>
2024-02-23 09:35:30 +0500

    repetitive using while loop

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from mapping import morse_code_dict
 
-user_input = input('Enter any text to translate: ')
-translated = ''
-for char in user_input.strip():
-    translated += morse_code_dict[char.upper()]
+
+while True:
+    user_input = input('Enter any text to translate: ')
+    translated = ''
+    for char in user_input.strip():
+        translated += morse_code_dict[char.upper()]
 
 
-print(translated)
+    print(translated)
