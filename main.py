b260a9168d745cd2d3310040149248b90ac38ed2
Awwab <mawwabkhank2006@gmail.com>
2024-02-23 09:39:10 +0500

    user choice to translate

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from mapping import morse_code_dict
 
 
 while True:
-    user_input = input('Enter any text to translate: ')
-    translated = ''
-    for char in user_input.strip():
-        translated += morse_code_dict[char.upper()]
+    choice = int(input('Type 1 to translate text to morse code.\nType 2 to translate morse code to text.\nType q to exit.\n'))
+    if choice == 1:
+        user_input = input('Enter the text: ')
+        translated = ''
+        for char in user_input.strip():
+            translated += morse_code_dict[char.upper()]
 
 
-    print(translated)
+        print(translated)
