40b639f4215b904606322bcade3a5c81a45d4a10
Awwab <mawwabkhank2006@gmail.com>
2024-02-23 09:49:46 +0500

    any key to quit the program

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from mapping import morse_code_dict
 
 
 while True:
-    choice = int(input('Type 1 to translate text to morse code.\nType 2 to translate morse code to text.\nType q to exit.\n'))
-    if choice == 1:
+    choice = input('Type 1 to translate text to morse code.\nType 2 to translate morse code to text.\nType q to exit.\n')
+    if choice == '1':
         user_input = input('Enter the text: ')
         translated = ''
         for char in user_input.strip():
@@ -13,10 +13,13 @@ while True:
         print(translated)
 
     
-    elif choice == 2:
+    elif choice == '2':
         user_input = input('Enter morse code: ')
         translated = ''
         for char in user_input.split():
             tchar = list(morse_code_dict.keys())[list(morse_code_dict.values()).index(char)]
             translated += tchar
         print(translated)
+
+    else:
+        break
